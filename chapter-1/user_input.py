b1288b5ae70e6eb755388e15d5e542c79d0e3fd0
Saqlain Sajjad <sajjadsaqlain298@gmail.com>
2026-03-26 09:41:36 +0500

    Add user input functionality in user_input.py

---
+++ chapter-1/user_input.py
@@ -0,0 +1,7 @@
+#input function with varible  dynamic or static 
+#name
+Name=input("what is your name? ") 
+#country 
+country=input("where are you from? ")
+#print
+print ("hello",Name,"\nfrom",country) 
