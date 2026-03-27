72dbf83da93f0a4140fc04707d81d7bed2a78745
Saqlain Sajjad <sajjadsaqlain298@gmail.com>
2026-03-27 21:23:19 +0500

    Add files via upload

---
+++ chapter-1/datatype.py
@@ -0,0 +1,46 @@
+#data type 
+a=10 #integer data type
+b=10.5 #float data type 
+c='saqlain' #string data type
+d=True #boolean data type   boolean data type can only have two values True or False first letter of boolean data type is always capital 
+e= None #none data type 
+f= [1,2,3,4,5] #list data type 
+g="1234" #string data type  
+h="" # blank string data type  
+i = " " # string data type with a space 
+
+#practice some important functions
+number = 10
+text = "string"
+print (number)
+print (text)
+print (type(number)) #type function is used to check the data type of a variable
+print (type(text))
+#print (len(number)) #len function is used to check the length of a string
+#print (len(text))   
+# string classes 
+print ("saqlain".upper()) #upper function is used to convert a string to uppercase
+print ("saqlain".lower()) #lower function is used to convert a string to lowercase    
+print (text.upper()) # in classes we have to enter  a value first then classes methode  
+print (number.bit_length) 
+ 
+#challenge 
+age =(22)
+height= (5.10)
+name = ("saqlain")
+is_student = (True) 
+bodycount = (None) 
+#print values
+print ("my name is ",name)
+print ("my age is ",age)    
+print ("my height is ",height)  
+print ("am i a student? ",is_student)
+print ("my body count is ",bodycount) 
+#print data types
+print (type(age))
+print (type(height))
+print (type(name))
+print (type(is_student))
+print (type(bodycount))
+#print length of string 
+print(len(name))   
